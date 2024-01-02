eef3c4763328b9ee181e4daaa1ddc2ba60233dcb
Andrii <andriidubyna03@gmail.com>
2024-01-02 21:54:09 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const storeName = 'Alice'
+
+const storeDescription = {
+  budget: 10000,
+  employees: ['Andry', 'Max', 'Masha']
+}
+
+const products = {
+  telephone: 1000,
+  tv: 6000
+}
+
+storeDescription.products;
+
+console.log(storeDescription);
